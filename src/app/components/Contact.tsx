8476f54cdfa5e6e68b59350bb32849a9053262c3
Pedro Correia <pchenr1que1@gmail.com>
2024-05-07 06:11:12 -0300

    skeleton finished

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,13 +2,15 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-alert */
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { BiPaperPlane } from 'react-icons/bi';
-import { BsClipboard, BsWhatsapp } from 'react-icons/bs';
+import { BsClipboard, BsGithub, BsLinkedin, BsWhatsapp } from 'react-icons/bs';
 import { copyToClipboard } from '../utils/clipboard';
 import Toast from './Toast';
 
 export default function Contact(): React.ReactElement {
+  const [showToast, setShowToast] = useState(false);
+
   return (
     <div
       className='min-h-lvh w-full flex flex-col bg-bg p-36 gap-2'
@@ -17,7 +19,6 @@ export default function Contact(): React.ReactElement {
       <h1 className='font-Raleway text-white text-5xl font-extrabold self-center'>
         Contact me
       </h1>
-
       <div className='flex flex-row w-full h-fit justify-evenly mt-32 mb-auto align-center'>
         <Link href='https://wa.me/+5582999303230' target='_blank'>
           <button
@@ -31,7 +32,11 @@ export default function Contact(): React.ReactElement {
 
         <div
           className='flex flex-col gap-2 items-center cursor-pointer'
-          onClick={() => copyToClipboard('phrcorreia3392@gmail.com')}
+          onClick={() => {
+            copyToClipboard('phrcorreia3392@gmail.com');
+
+            setShowToast(true);
+          }}
         >
           <BiPaperPlane className='text-herolayer opacity-80 text-3xl' />
           <p className='text-herolayer text-base font-Raleway'>
@@ -40,8 +45,30 @@ export default function Contact(): React.ReactElement {
           <BsClipboard className='text-herolayer opacity-80 text-3xl mt-4' />
         </div>
       </div>
+      {showToast && (
+        <Toast setShowToast={setShowToast} message='Email copied!' />
+      )}
 
-      <Toast message='test' />
+      <div className='absolute bottom-0 left-0 w-full h-24 bg-[#14142b] flex flex-row items-center px-4'>
+        <p className='font-Raleway text-white opacity-80 text-base'>
+          Developed by Pedro Correia, base on design by
+          <Link
+            href='https://www.figma.com/community/file/1162385628597290713/my-web-portfolio'
+            target='_blank'
+          >
+            {' '}
+            Henrique Sousa
+          </Link>
+        </p>
+        <div className='flex w-fit h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-6'>
+          <Link href='https://www.linkedin.com/in/phrcorreia/' target='_blank'>
+            <BsLinkedin className='text-white text-2xl mt-4 cursor-pointer opacity-50' />
+          </Link>
+          <Link href='https://github.com/pdr3392' target='_blank'>
+            <BsGithub className='text-white text-2xl mt-4 cursor-pointer opacity-50' />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/components/Toast.tsx
+++ src/app/components/Toast.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 
 export default function Toast({
   message,
+  setShowToast,
 }: {
   message: string;
+  setShowToast: React.Dispatch<React.SetStateAction<boolean>>;
 }): React.ReactElement {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      setTimeout(() => setShowToast(false), 500);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className='fixed w-full h-full pointer-events-none top-0 left-0 items-end justify-center flex'>
+    <div
+      className={`fixed w-full h-full pointer-events-none top-0 left-0 items-end justify-center flex transition-opacity duration-500 ${
+        visible ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
       <div
         id='toast-success'
         className='flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800'
@@ -30,6 +49,7 @@ export default function Toast({
           className='ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
           data-dismiss-target='#toast-success'
           aria-label='Close'
+          onClick={() => setVisible(false)}
         >
           <span className='sr-only'>Close</span>
           <svg
