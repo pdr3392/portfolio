3ca070149572b83a5d151e449e377a89603e2150
Pedro Correia <pchenr1que1@gmail.com>
2024-05-08 05:02:22 -0300

    dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,23 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import * as React from 'react';
+import dynamic from 'next/dynamic';
 import { usePortfolioOrRantState } from './hooks/usePortfolioOrRant';
-import Portfolio from './layouts/Portfolio';
-import Rant from './layouts/Rant';
+
+const DynamicPortfolio = dynamic(() => import('./layouts/Portfolio'), {
+  ssr: false,
+});
+
+const DynamicRant = dynamic(() => import('./layouts/Rant'), {
+  ssr: false,
+});
 
 export default function Home(): React.ReactElement {
   const { portfolioOrRant } = usePortfolioOrRantState();
 
   if (portfolioOrRant === 'portfolio') {
-    return <Portfolio />;
+    return <DynamicPortfolio />;
   }
 
-  return <Rant />;
+  return <DynamicRant />;
 }
